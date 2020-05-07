98ee60e786f0336c309815660bd9a1554e336de8
Ivan Danilov <ivandanilov996@yandex.ru>
2020-05-07 16:21:45 +0300

    hw 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,46 @@
+"use sttrict";
+
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+  if (animal === undefined) {
+    return null;
+  }
+
+  const sound = animal.sound;
+
+  return (animalSound = sound);
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  let average = 0;
+
+  if (marks.length === 0) {
+    return 0;
+  }
+
+  for (let element of marks) {
+    average += +element;
+  }
+
+  average /= marks.length;
+
+  const roundedAverage = Math.round(average);
+
+  return (averageMark = roundedAverage);
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  const now = Date.now();
+
+  const date = new Date(birthday);
+
+  birthday = +date;
+
+  const diff = now - birthday;
+
+  // в году 8760 часов + 24 каждые 4 года = 8766
+  const age = diff / 1000 / 3600 / 8766;
+
+  verdict = age > 18 ? true : false;
+
+  return verdict;
 }
